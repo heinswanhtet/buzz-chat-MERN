7269c7caecdf36b02559e9b73b74d1ff6239945c
Hein Swan Htet <hshscientist@gmail.com>
2024-04-05 15:20:07 +0630

    Added functionality to send message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,5 +1,38 @@
+const User = require('../models/User')
+const Message = require('../models/Message')
+const Chat = require('../models/Chat')
+const CustomError = require('../errors')
+const { StatusCodes } = require('http-status-codes')
+
 const sendMessage = async (req, res) => {
-    res.send('send message')
+    const { userId: senderId } = req.user
+    const { id: receiverId } = req.params
+    const { message } = req.body
+
+    if (!message)
+        throw new CustomError.BadRequestError('Please provide message to send')
+
+    const userExist = await User.findOne({ _id: receiverId })
+    if (!userExist)
+        throw new CustomError.NotFoundError('Receiver user does not exist')
+
+    const newMessage = await Message.create({
+        sender: senderId,
+        receiver: receiverId,
+        message
+    })
+
+    let chat = await Chat.findOne({
+        participants: { $all: [senderId, receiverId] }
+    })
+
+    if (!chat)
+        chat = await Chat.create({ participants: [senderId, receiverId] })
+
+    chat.messages.push(newMessage._id)
+    await chat.save()
+
+    res.status(StatusCodes.CREATED).json({ message: newMessage })
 }
 
 const getMessage = async (req, res) => {
