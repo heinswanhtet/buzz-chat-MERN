382271c4817c911546e22d033c0e1a115cbb715d
Hein Swan Htet <hshscientist@gmail.com>
2024-04-06 12:25:38 +0630

    Added elements and styled for Login page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,6 +1,48 @@
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
-export default Login
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10">
+        <h1 className="text-3xl font-semibold text-center text-gray-700">
+          Login <span className="text-pink-400">Buzz Chat</span>
+        </h1>
+
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter email"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter password"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+
+          <p className="text-sm  mt-2 inline-block">
+            Don't have an account?{" "}
+            <span className="hover:text-blue-400 cursor-pointer">Register</span>
+          </p>
+
+          <button
+            type="submit"
+            className="btn btn-block btn-secondary btn-sm mt-2"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default Login;
