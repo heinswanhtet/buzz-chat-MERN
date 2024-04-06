7a53410af9a488ddf4286ea8f7eff883643216b9
Hein Swan Htet <hshscientist@gmail.com>
2024-04-06 22:19:11 +0630

    Created AuthContext

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,59 @@
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  const saveUser = (user) => {
+    setUser(user);
+  };
+
+  const removeUser = () => {
+    setUser(null);
+  };
+
+  const fetchUser = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/users/show-me");
+      saveUser(data.user);
+    } catch (error) {
+      removeUser();
+    }
+    setIsLoading(false);
+  };
+
+  const logoutUser = async () => {
+    try {
+      await axios.delete("/api/v1/auth/logout");
+      removeUser();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoading,
+        saveUser,
+        user,
+        logoutUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
+
+export { AuthProvider };
