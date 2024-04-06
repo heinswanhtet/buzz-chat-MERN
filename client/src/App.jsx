e3cfa5eb09e7288d62344b1f944135e107785b9a
Hein Swan Htet <hshscientist@gmail.com>
2024-04-06 22:34:58 +0630

    Added loading to initial check whether or not user logged in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,40 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
 import { Home, Login, Register } from "./pages";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
+  const { user, isLoading } = useAuthContext();
+
+  if (isLoading) {
+    return (
+      <div className="p-4 h-screen flex justify-center items-center">
+        <span className="loading loading-spinner text-secondary"></span>
+      </div>
+    );
+  }
+
   return (
     <div className="p-4 h-screen flex justify-center items-center">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={user ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/register"
+            element={user ? <Navigate to="/" /> : <Register />}
+          />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/" /> : <Login />}
+          />
         </Routes>
       </Router>
     </div>
