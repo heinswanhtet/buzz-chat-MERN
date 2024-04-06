347870f432189721cbbd0196cb5a03afdc609957
Hein Swan Htet <hshscientist@gmail.com>
2024-04-06 13:03:01 +0630

    Added styling to home.jsx

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
   return (
-    <div>
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10">
       <Sidebar />
       <MessageContainer />
     </div>
