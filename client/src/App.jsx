e196a52ac266048fa52d6d60deb9833e97c967da
Hein Swan Htet <hshscientist@gmail.com>
2024-04-06 11:17:34 +0630

    Setup baisc structure for sing up and login pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './pages/login/Login'
+import SignUp from './pages/signUp/SignUp'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <button className="btn btn-neutral">Neutral</button>
-    </>
+    <div className='p-4 h-screen flex justify-center items-center'>
+      <Login />
+      {/* <SignUp /> */}
+    </div>
   )
 }
 
