1f770f99659679b2bf447b03a14c1cd8504102a1
Hein Swan Htet <hshscientist@gmail.com>
2024-04-06 15:29:34 +0630

    Added elements and styles to messages

--- client/src/components/messages/MessageContainer.jsx
+++ client/src/components/messages/MessageContainer.jsx
@@ -4,7 +4,7 @@ import Messages from "./Messages";
 const MessageContainer = () => {
   return (
     <div className="md:min-w-[550px] flex flex-col">
-      <div className="bg-slate-400 px-4 py-2 mb-2">
+      <div className="bg-[#2b3440] px-4 py-2 mb-2">
         <span className="label-text text-white">To:</span>{" "}
         <span className="text-white font-bold">Phone Htet Myint</span>
       </div>
--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -1,4 +1,15 @@
+import Message from "./Message";
+
 const Messages = () => {
-  return <div>Messages</div>;
+  return (
+    <div className="flex-1 px-2 overflow-auto">
+      <Message placing={"chat-start"} />
+      <Message placing={"chat-end"} />
+      <Message placing={"chat-start"} />
+      <Message placing={"chat-end"} />
+      <Message placing={"chat-start"} />
+      <Message placing={"chat-end"} />
+    </div>
+  );
 };
 export default Messages;
