a323c044fddf3c25a5183e551817174468df531e
Hein Swan Htet <hshscientist@gmail.com>
2024-04-06 21:40:12 +0630

    Added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Home, Login, Register } from "./pages";
 
 function App() {
   return (
     <div className="p-4 h-screen flex justify-center items-center">
-      {/* <Register /> */}
-      {/* <Login /> */}
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
