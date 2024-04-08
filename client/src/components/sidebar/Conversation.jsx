c5874872f1934da642c7915dbff3d5b84b3aa0be
Hein Swan Htet <hshscientist@gmail.com>
2024-04-08 11:36:22 +0630

    Fixed some stylings for sidebar

--- client/src/components/sidebar/Conversation.jsx
+++ client/src/components/sidebar/Conversation.jsx
@@ -1,23 +1,23 @@
 const Conversation = () => {
-  return (
-    <>
-      <div className="flex gap-3 items-center justify-between transition-all hover:bg-slate-400 rounded px-2 py-1.5 cursor-pointer">
-        <div className="avatar online placeholder">
-          <div className="bg-neutral text-neutral-content rounded-full w-10">
-            <span className="text-xl">H</span>
-          </div>
-        </div>
+    return (
+        <>
+            <div className="flex gap-3 items-center justify-between transition-all hover:bg-slate-400 hover:text-white rounded px-2 py-1.5 cursor-pointer">
+                <div className="avatar online placeholder">
+                    <div className="bg-neutral text-neutral-content rounded-full w-10">
+                        <span className="text-xl">H</span>
+                    </div>
+                </div>
 
-        <div className="flex-1">
-          <div className="flex gap-24 justify-between">
-            <p className="text-lg">Hein Swan Htet</p>
-            <span>😽</span>
-          </div>
-        </div>
-      </div>
+                <div className="flex-1">
+                    <div className="flex gap-24 justify-between">
+                        <p className="text-lg">Hein Swan Htet</p>
+                        {/* <span>😽</span> */}
+                    </div>
+                </div>
+            </div>
 
-      <div className="divider my-0 py-0 h-3" />
-    </>
-  );
-};
-export default Conversation;
+            <div className="divider my-0 py-0 h-3" />
+        </>
+    )
+}
+export default Conversation
--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,14 @@
-import Conversations from "./Conversations";
-import LogoutButton from "./LogoutButton";
-import SearchInput from "./SearchInput";
+import Conversations from "./Conversations"
+import LogoutButton from "./LogoutButton"
+import SearchInput from "./SearchInput"
 
 const Sidebar = () => {
-  return (
-    <div className="border-r border-slate-500 p-4 flex flex-col">
-      <SearchInput />
-      <Conversations />
-      <LogoutButton />
-    </div>
-  );
-};
-export default Sidebar;
+    return (
+        <div className="border-r border-slate-500 p-4 w-[25%] flex flex-col">
+            <SearchInput />
+            <Conversations />
+            <LogoutButton />
+        </div>
+    )
+}
+export default Sidebar
