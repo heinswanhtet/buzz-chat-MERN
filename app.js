6c6767e8ab03dcdc3ef673edeb95fdd9637b752f
Hein Swan Htet <hshscientist@gmail.com>
2024-04-09 09:37:46 +0630

    Created socket sever on express server

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("express-async-errors")
 
 // express
 const express = require("express")
-const app = express()
+const { app, server } = require("./socket/socket")
 
 // utility packages
 const morgan = require("morgan")
@@ -71,7 +71,7 @@ const port = process.env.PORT || 3000
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
-        app.listen(port, () => {
+        server.listen(port, () => {
             console.log(`Server is listening on port ${port}...`)
         })
     } catch (error) {
---
+++ socket/socket.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+
+const http = require("http")
+const server = http.createServer(app)
+
+const { Server } = require("socket.io")
+const io = new Server(server, {
+    cors: {
+        origin: ["http://localhost:3000"],
+        methods: ["GET", "POST"],
+    },
+})
+
+io.on("connection", (socket) => {
+    console.log(`a user connected socket: ${socket.id}`)
+
+    socket.on("disconnect", () => {
+        console.log(`a user disconnected socket: ${socket.id}`)
+    })
+})
+
+module.exports = {
+    app,
+    io,
+    server,
+}
