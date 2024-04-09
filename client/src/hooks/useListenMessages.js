6d4b91a7124ed1426fe2024e53bfc439908b657d
Hein Swan Htet <hshscientist@gmail.com>
2024-04-09 11:44:34 +0630

    Added sound when new message comes in

--- client/src/hooks/useListenMessages.js
+++ client/src/hooks/useListenMessages.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 import { useSocketContext } from "../context/SocketContext"
 import useConversation from "../zustand/useConversation"
+import notificationSound from "../assets/sounds/notification.mp3"
 
 const useListenMessages = () => {
     const { socket } = useSocketContext()
@@ -12,6 +13,8 @@ const useListenMessages = () => {
         socket?.on("newMessage", (newMessage) => {
             // console.log("inner", messages)
             newMessage.shake = true
+            const sound = new Audio(notificationSound)
+            sound.play()
             setMessages([...messages, newMessage])
         })
 
